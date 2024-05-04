24db74df94e114db138391afac42f014162ac2aa
snipes <schneidsmc@gmail.com>
2024-05-04 16:17:21 -0500

    resolvers addUserInfo

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,63 +1,61 @@
-const {Food, User} = require("../models");
-const {signToken, AuthenticationError } = require('../utils/auth');
+const { Food, User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find().populate('foods')
+      return User.find().populate("foods");
     },
     user: async (parent, { username }) => {
-        return User.findOne({ username }).populate('foods');
+      return User.findOne({ username }).populate("foods");
     },
     foods: async (parent, { username }) => {
-        const params = username ? { username } : {};
-        return Food.find(params).sort({ createdAt: -1})
+      const params = username ? { username } : {};
+      return Food.find(params).sort({ createdAt: -1 });
     },
     food: async (parent, { foodId }) => {
-        return Food.findOne({ _id: foodId});
+      return Food.findOne({ _id: foodId });
     },
-    me: async (parent, args, context ) => {
-        if (context.user) {
-            return User.findOne({ _id: context.user._id}).populate('foods');
-        }
-        throw AuthenticationError;
-    }
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("foods");
+      }
+      throw AuthenticationError;
+    },
   },
 
   Mutation: {
-    addUser: async (parent, {username, email, password }) => {
-        const user = await User.create({username, email, password });
-        const token = signToken(user);
-        return { token, user };
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-    login: async (parent, {email, password }) => {
-        const user = await User.findOne({ email });
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-        if (!user) {
-            throw AuthenticationError;
-        }
+      if (!user) {
+        throw AuthenticationError;
+      }
 
-        const correctPw = await user.isCorrectPassword(password);
+      const correctPw = await user.isCorrectPassword(password);
 
-        if (!correctPw) {
-            throw AuthenticationError;
-        }
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
 
-        const token = signToken(user);
+      const token = signToken(user);
 
-        return {token, user};
+      return { token, user };
     },
-    addUserInfo: async (parent, {height, weight, age, sex}, context) => {
-        if (context.user) {
-            await User.findOneAndUpdate(
-                { _id: context.user._id },
-                { $addToSet: {height: height}},
-                { $addToSet: {weight: weight}},
-                { $addToSet: {age: age}},
-                { $addToSet: {sex: sex}},
-            )
-        }
-    }
-  }
+    addUserInfo: async (parent, { height, weight, age, sex }, context) => {
+      if (context.user) {
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $set: { height, weight, age, sex } },
+          { new: true },
+        );
+      }
+    },
+  },
 };
 
 module.exports = resolvers;

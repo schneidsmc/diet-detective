a0b0ac5fe8d520d5a35ecea9a0d348536aceaf36
Travis Umbel <Umbelth94@gmail.com>
2024-05-04 14:09:12 -0500

    add full food schema

--- server/models/Food.js
+++ server/models/Food.js
@@ -4,6 +4,25 @@ const foodSchema = new Schema({
     name: {
         type: String,
         required: true,
+    },
+    categories: {
+        calories: Number,
+        sodium: String,
+        sugar: String,
+        saturated_fats: String,
+        unsaturated_fats: String,
+        protein: String,
+        macronutrients: {
+            carbohydrate: String,
+            fiber: String,
+            total_fats: String
+        },
+        micronutrients: {
+            vitamin_A: String,
+            vitamin_C: String,
+            calcium: String,
+            iron: String
+        }
     }
 });
 

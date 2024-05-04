0749d8c95ebf93ad5f3db068c546d5e0e0dcdc4b
Travis Umbel <Umbelth94@gmail.com>
2024-05-04 14:52:17 -0500

    saving before pull

--- server/models/User.js
+++ server/models/User.js
@@ -21,19 +21,15 @@ const userSchema = new Schema({
     },
     height: {
         type: Number,
-        required: true,
     },
     weight: {
         type: Number,
-        required: true
     },
     age: {
         type: Number,
-        required: true
     }, 
     sex: {
         type: String,
-        required: true
     },
     foods: [
         {
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,57 @@
+const typeDefs = gql`
+    type User {
+        _id: ID 
+        username: String!
+        email: String!
+        password: String!
+        foods: [Food]!
+    }
+
+    type Macronutrients {
+        carbohydrates: String
+        fiber: String
+        total_fats: String
+    }
+
+    type Micronutrients {
+        vitamin_A: String
+        vitamin_C: String
+        calcium: String
+        iron: String
+    }
+    
+    type Food {
+        _id: ID!
+        name: String
+        categories: Foodcategories
+    }
+
+    type FoodCategories {
+        calories: Number
+        sodium: String
+        sugar: String
+        saturated_fats: String
+        unsaturated_fats: String
+        protein: String
+        macronutrients: Macronutrients
+        micronutrients: Micronutrients
+    }
+    
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        users:[User]
+        user(username: String!): User
+        foods(username: String): [Food]
+        food(foodId: ID!): Food
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        
+    }
+    `

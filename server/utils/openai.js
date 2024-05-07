4701e15f3c44ef7b03d64f3d5f800e64cd50a2cd
snipes <schneidsmc@gmail.com>
2024-05-06 21:02:05 -0500

    openai api

--- server/utils/openai.js
+++ server/utils/openai.js
@@ -9,8 +9,7 @@ async function getNutrition(userFoodInput) {
     prompt:
       "Respond only using this JSON object with the properties included in this JSON object.  Respond with only valid JSON",
     question: `List the nutrition facts for ${userFoodInput} to include calories, protein, saturated fats, sodium, sugar, unsaturated fats, micronutrients and macronutrients. Micronutrients should include
-        calcium, iron, vitamin A and vitamin C. Macronutrients should include carbohydrates, fiber and total fats.`,
-    // activities: [],
+        calcium, iron, vitamin A and vitamin C. Macronutrients should include carbohydrates, fiber and total fats. Always use the generic brand nutrition information`,
   };
   console.log("NutritionPrompt", NutritionPrompt);
   const NutritionResponse = await openai.chat.completions.create({
@@ -23,7 +22,6 @@ async function getNutrition(userFoodInput) {
       },
       {
         role: "user",
-        // content: `List 5 things with a description and links for activties to do Green Bay, Wisconsin if the focus of the trip is camping in January`,
         content: JSON.stringify(NutritionPrompt),
       },
     ],
@@ -31,5 +29,5 @@ async function getNutrition(userFoodInput) {
   console.log("GPT RESPONSE", NutritionResponse.choices[0].message);
 }
 
-const userFoodInput = "cheeseburger";
+const userFoodInput = "animal crackers";
 getNutrition(userFoodInput);

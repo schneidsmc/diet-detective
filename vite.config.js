6f0aea97ef973519a31b6f70e9f85959f8544efb
Travis Umbel <Umbelth94@gmail.com>
2024-05-12 19:51:23 -0500

    move .env file for server start, added .nvmrc and .node-version file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on 'mode' in the current working directory.
+  // Set the third paramter to '' to load all env
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env.OPENAI_API_KEY": JSON.stringify(env.OPENAI_API_KEY),
+    },
+    plugins: [react()],
+  };
+});

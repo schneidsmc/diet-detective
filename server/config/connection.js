5295a35664ecc94bc433b3d3c64df20d988dcd43
Travis Umbel <Umbelth94@gmail.com>
2024-05-12 21:49:24 -0500

    added in noted version of our old mongodb connection string in case it is needed for you guys to test

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
+// you can uncomment this if the app now has trouble connecting to the mongoDB
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/diet-detective');
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/diet-detective",
 );

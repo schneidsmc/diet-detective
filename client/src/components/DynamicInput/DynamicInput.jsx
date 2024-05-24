3dae9b4682bc10ee6e02aa975a0d5864b85c380f
Travis Umbel <Umbelth94@gmail.com>
2024-05-24 15:35:11 -0500

    settings for render deploy

--- client/src/components/DynamicInput/DynamicInput.jsx
+++ client/src/components/DynamicInput/DynamicInput.jsx
@@ -9,8 +9,8 @@ async function fetchDataFromOpenAI(foodInputs) {
     // API will use the base URL from environment variables (which we will put in .env once the site is hosted) OR localhost:3000
 
     //Uncomment this line instead when running in development
-    const baseURL = "http://localhost:3001";
-    // const baseURL = process.env.REACT_APP_BASE_URI; //Comment this out when running in development
+    // const baseURL = "http://localhost:3001";
+    const baseURL = process.env.REACT_APP_BASE_URI; //Comment this out when running in development
     const queryParams = new URLSearchParams({
       foodInputs: JSON.stringify(foodInputs),
     });

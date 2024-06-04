6deea5be0345de36ccd8989c953d52d6d8da8a58
Travis Umbel <Umbelth94@gmail.com>
2024-06-03 19:32:04 -0500

    added simple placeholder alert so that the user knows that our "save mealplan" functionality is not currently working

--- client/src/components/MealPlanCrud/CreateMealPlan.jsx
+++ client/src/components/MealPlanCrud/CreateMealPlan.jsx
@@ -5,6 +5,8 @@ import { CREATE_MEALPLAN } from "../../utils/mutations";
 const CreateMealPlanComponent = ({ userToken }) => {
   const [createMealPlan] = useMutation(CREATE_MEALPLAN);
 
+  const handleCreateMealPlanPlaceHolder = () =>
+    alert("This functionality is not currently available :(");
   const handleCreateMealPlan = async () => {
     try {
       const currentDate = new Date().toISOString().split("T")[0]; // Get current date
@@ -26,7 +28,9 @@ const CreateMealPlanComponent = ({ userToken }) => {
   return (
     <button
       className="block mt-10 mb-5 mx-auto text-3xl border-solid border-white dark:hover:bg-gray-800 dark:bg-gray-600 border-2 rounded-2xl py-3 pb-4 px-8 shadow-button font-bold"
-      onClick={handleCreateMealPlan}
+      // onClick={handleCreateMealPlan}
+      // This code is currently just a placeholder alert
+      onClick={handleCreateMealPlanPlaceHolder}
     >
       Create Meal Plan
     </button>
